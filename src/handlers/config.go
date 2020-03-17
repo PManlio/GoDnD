81129bfbf189bb6e28dcf7c67c718ff6404fced9
pmanlio <manpuglisi94@gmail.com>
2020-03-17 23:17:18 +0100

    char creation (class insertion done) wip, added chat parser. Next: char abilities

--- src/handlers/config.go
+++ src/handlers/config.go
@@ -1,7 +1,6 @@
 package handlers
 
 // get this from environment.yaml
-//const token string = "Njg4NDMyMDUyMjY5MjE5OTMw.Xm0PaQ.fm_Is5nrNRCIYl3PBl5BZG6fMVo"
 const token string = "Njg4NDMyMDUyMjY5MjE5OTMw.XnFHEA.qdawgBabQMcQSIDJQxcwYgNFdoY"
 const BotPrefix string = "!"
 const ChannelID string = "688419377816797192"

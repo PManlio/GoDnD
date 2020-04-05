bddb2049fc7c62bf14043d9f7e02dd278f7e98c2
manlio <manpuglisi94@gmail.com>
2020-04-05 21:17:12 +0200

    working on throws

--- src/handlers/config.go
+++ src/handlers/config.go
@@ -1,7 +1,7 @@
 package handlers
 
 // TODO: get this from environment.yaml
-const token string = "Njg4NDMyMDUyMjY5MjE5OTMw.XojPlA.jTO-tFCfvN0rjionxomujbPnGQQ"
+const token string //= "Njg4NDMyMDUyMjY5MjE5OTMw.XooiEg.KGcSIjS9c4PXRCa9KTIREwIhQeg"
 const BotPrefix string = "!"
 const ChannelID string = "688419377816797192"
 
